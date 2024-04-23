7eac7cba0f789e1769c1e1aaccbb64e326ca6e90
Fatima Ali <fatimaaliforwork@gmail.com>
2024-04-23 11:12:55 +0500

    adds pricing tables

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -1,4 +1,16 @@
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { pricingCards } from "@/lib/constants";
+import clsx from "clsx";
+import { Check } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+import { features } from "process";
 
 export default function Home() {
   return (
@@ -8,7 +20,7 @@ export default function Home() {
 
         <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] -z-10" />
 
-        <p className="text-center mt-32">Run your agency, in one place</p>
+        <p className="text-center mt-96">Run your agency, in one place</p>
         <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
           <h1 className="text-9xl font-bold text-center md:text-[300px]">
             Plura
@@ -22,17 +34,63 @@ export default function Home() {
             width={1200}
             className="rounded-tl-2xl rounded-tr-2xl border-2 border-muted"
           />
-          {/* <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10"></div> */}
+          <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background  left-0 right-0 absolute z-10"></div>
         </div>
-        <section className="flex justify-center items-center flex-col gap-4 md:!mt-20 mt-[-60px]">
-          <h2 className="text-4xl text-center"> Choose what fits you right</h2>
-          <p className="text-muted-foreground text-center">
-            Our straightforward pricing plans are tailored to meet your needs.
-            If
-            {" you're"} not <br />
-            ready to commit you can get started for free.
-          </p>
-        </section>
+      </section>
+      <section className="flex justify-center items-center flex-col gap-4 md:!mt-96 mt-[260px]">
+        <h2 className="text-4xl text-center"> Choose what fits you right</h2>
+        <p className="text-muted-foreground text-center">
+          Our straightforward pricing plans are tailored to meet your needs. If
+          {" you're"} not <br />
+          ready to commit you can get started for free.
+        </p>
+
+        <div className="flex  justify-center gap-4 flex-wrap mt-6">
+          {pricingCards.map((card) => (
+            <Card
+              key={card.title}
+              className={clsx(`w-[300px] flex flex-col justify-between `, {
+                "border-2 border-primary": card.title === "Unlimited Saas",
+              })}
+            >
+              <CardHeader>
+                <CardTitle
+                  className={clsx(``, {
+                    "text-muted-foreground": card.title !== "Unlimited Saas",
+                  })}
+                >
+                  {card.title}
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <span className="font-bold text-4xl">{card.price}</span>
+                <span className="text-muted-foreground">/m</span>
+              </CardContent>
+              <CardFooter className=" flex flex-col gap-4 items-start">
+                <div>
+                  {card.features.map((feature) => (
+                    <div className="flex gap-2 items-center">
+                      <Check className="text-muted-foreground"></Check>
+                      <p> {feature}</p>
+                    </div>
+                  ))}
+                </div>
+                <Link
+                  href={`/agency/${card.priceId}`}
+                  className={clsx(
+                    `w-full text-center bg-primary p-2 rounded-md`,
+                    {
+                      "!bg-muted-foreground": card.title === "Unlimited Saas",
+                    }
+                  )}
+                >
+                  {" "}
+                  Get Started
+                </Link>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
       </section>
     </>
   );
