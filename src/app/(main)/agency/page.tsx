d9164ea50108afb342db85db8480a96bf0d89828
Fatima Ali <fatimaaliforwork@gmail.com>
2024-04-25 09:15:59 +0500

    integrates a form to get agency details inside agency page

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -2,8 +2,10 @@
 import { getAuthUserDetails, verifyAndAcceptInvitation } from "@/lib/queries";
 import { Plan } from "@prisma/client";
 import { redirect } from "next/navigation";
+import AgencyDetails from "../../../components/forms/AgencyDetails";
 
 import React from "react";
+import { currentUser } from "@clerk/nextjs";
 
 const Page = async ({
   searchParams,
@@ -34,13 +36,14 @@ const Page = async ({
       return <div>Not authorized</div>;
     }
   }
+  const authUser = await currentUser();
   return (
     <div className="flex justify-center items-center mt-4">
       <div className="max-w-[850px] border-[1px] p-4 rounded-xl">
         <h1 className="text-4xl"> Create An Agency</h1>
-        {/* <AgencyDetails
+        <AgencyDetails
           data={{ companyEmail: authUser?.emailAddresses[0].emailAddress }}
-        /> */}
+        />
       </div>
     </div>
   );
